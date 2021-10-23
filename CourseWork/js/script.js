ec6ee32a4939f36f4c244faf5a0e82a423830a6d
skarpovv <72975289+skarpovv@users.noreply.github.com>
2021-10-24 02:45:25 +0300

    Added First Alghoritm

--- CourseWork/js/script.js
+++ CourseWork/js/script.js
@@ -1,9 +1,33 @@
 document.querySelector('#buttoncode').onclick = CodeFunction;
+document.querySelector('#buttondecode').onclick = DecodeFunction;
 
 function CodeFunction()
 {
-    let text = document.querySelector('#simpletext');
-    console.log(text.value);
+    document.querySelector('#ans').value = '';
+    let text = document.querySelector('#simpletext').value;
+    let key = document.querySelector('#key1').value;
+    console.log("I'm key = "+key + " " + typeof(key));
+    document.querySelector('#key2').value = key;
 
+    let codestr = '';
+    for (let i = 0; i < text.length; i++)
+    {
+        codestr += String.fromCharCode((text[i].charCodeAt())^Number(key));
+    }
+    console.log(codestr);
+    document.querySelector('#codetext').value = codestr;
 }
 
+String.fromCharCode("D".charCodeAt()^12)
+
+function DecodeFunction()
+{
+    let text = document.querySelector('#codetext').value;
+    let key = document.querySelector('#key2').value;
+    let decode = '';
+    for (let i = 0; i < text.length; i++)
+    {
+        decode += String.fromCharCode((text[i].charCodeAt())^Number(key));
+    }
+    document.querySelector('#ans').value = decode;
+}
