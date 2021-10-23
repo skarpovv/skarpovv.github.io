33a1347c2772132dd05eda49e1fb77ddbc58761a
skarpovv <72975289+skarpovv@users.noreply.github.com>
2021-10-24 00:04:31 +0300

    Added project

---
+++ CourseWork/js/script.js
@@ -0,0 +1,9 @@
+document.querySelector('#buttoncode').onclick = CodeFunction;
+
+function CodeFunction()
+{
+    let text = document.querySelector('#simpletext');
+    console.log(text.value);
+
+}
+
